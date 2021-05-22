4474bcbb8ab50413759a1e663405537b9645973b
tambi09 <ritylya09@gmail.com>
2021-05-22 10:45:48 -0400

    initial commit

---
+++ src/day1/HelloWorld.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world ");
+        System.out.println("Adding GitHub Now");
+        System.out.println("YAY STUFF ARE ON GITHUB");
+    }
+}
